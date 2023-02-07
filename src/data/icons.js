b75404a21c37cabe53accd7a4c283f7230c07d29
Thiago Gaia <thiagogaia@gmail.com>
2023-02-07 00:20:09 -0300

    icones && axios

---
+++ src/data/icons.js
@@ -0,0 +1,31 @@
+export let cartIcon = `<svg width="33" height="33" viewBox="0 0 33 33" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M23.7188 23.7188H8.99766L5.40117 3.94453C5.35909 3.70772 5.23556 3.49307 5.05196 3.33771C4.86835 3.18235 4.63621 3.09605 4.3957 3.09375H2.0625" stroke="#6A82BA" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+      <path d="M10.3125 28.875C11.7364 28.875 12.8906 27.7207 12.8906 26.2969C12.8906 24.873 11.7364 23.7188 10.3125 23.7188C8.88864 23.7188 7.73438 24.873 7.73438 26.2969C7.73438 27.7207 8.88864 28.875 10.3125 28.875Z" stroke="#6A82BA" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+      <path d="M23.7188 28.875C25.1426 28.875 26.2969 27.7207 26.2969 26.2969C26.2969 24.873 25.1426 23.7188 23.7188 23.7188C22.2949 23.7188 21.1406 24.873 21.1406 26.2969C21.1406 27.7207 22.2949 28.875 23.7188 28.875Z" stroke="#6A82BA" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+      <path d="M8.05664 18.5625H24.2473C24.7293 18.564 25.1965 18.3955 25.5666 18.0866C25.9368 17.7778 26.1862 17.3484 26.2711 16.8738L27.8438 8.25H6.1875" stroke="#6A82BA" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+    </svg>`
+
+export let closeIcon = `<svg width="33" height="33" viewBox="0 0 33 33" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M25.7812 7.21875L7.21875 25.7812" stroke="#6A82BA" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+    <path d="M25.7812 25.7812L7.21875 7.21875" stroke="#6A82BA" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+    </svg>
+    `
+
+export let arrowRightIcon = `<svg width="22" height="22" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M3.4375 11H18.5625" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+    <path d="M12.375 4.8125L18.5625 11L12.375 17.1875" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+    </svg>
+    `
+
+export let minusCircleIcon = `<svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M16 28C22.6274 28 28 22.6274 28 16C28 9.37258 22.6274 4 16 4C9.37258 4 4 9.37258 4 16C4 22.6274 9.37258 28 16 28Z" stroke="#F04465" stroke-width="2" stroke-miterlimit="10"/>
+    <path d="M11 16H21" stroke="#F04465" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+    </svg>
+    `
+
+export let plusCircleIcon = `<svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M16 28C22.6274 28 28 22.6274 28 16C28 9.37258 22.6274 4 16 4C9.37258 4 4 9.37258 4 16C4 22.6274 9.37258 28 16 28Z" stroke="#19DEBE" stroke-width="2" stroke-miterlimit="10"/>
+    <path d="M11 16H21" stroke="#19DEBE" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+    <path d="M16 11V21" stroke="#19DEBE" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+    </svg>
+    `
